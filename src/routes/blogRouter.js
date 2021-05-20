549793d4be0dd1a78c1abb6c4951e40edd5d7f44
jsshin <jsshin@salin.co.kr>
2021-05-20 17:21:10 +0900

    transaction Fisish

--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -1,116 +1,118 @@
-const { Router } = require('express');
-const blogRouter = Router();
-const { Blog, User } = require('../models');
-const { isValidObjectId } = require('mongoose');
-const { commentRouter } = require('./commentRouter');
+const { Router } = require("express")
+const blogRouter = Router()
+const { Blog, User } = require("../models")
+const { isValidObjectId } = require("mongoose")
+const { commentRouter } = require("./commentRouter")
 
-blogRouter.use('/:blogId/comment', commentRouter);
+blogRouter.use("/:blogId/comment", commentRouter)
 
-blogRouter.get('/', async (req, res) => {
-    try {
-        let { page } = req.query;
-        page = parseInt(page);
-        let blogs = await Blog.find({})
-            .sort({ updatedAt: -1 })
-            .skip(page * 3)
-            .limit(3);
-        // N+1 Problem 을 해결하기위한 poplutae 사용
-        //.populate([{ path: 'user' }, { path: 'comments', populate: { path: 'user' } }]);
-        // example
-        // .populate([
-        //     { path: "user", select: "name fullName"},
-        //     {
-        //         path: "comments",
-        //         select : "content",
-        //         match : { user : "lkadsnfalskdnf"},
-        //         populate: { path: "user", select : "name fullName"}
-        //     }
-        // ])
-        return res.status(200).send({ blogs });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ err: err.message });
-    }
-});
+blogRouter.get("/", async (req, res) => {
+  try {
+    let { page } = req.query
+    page = parseInt(page)
+    let blogs = await Blog.find({})
+      .sort({ updatedAt: -1 })
+      .skip(page * 3)
+      .limit(3)
+    // N+1 Problem 을 해결하기위한 poplutae 사용
+    //.populate([{ path: 'user' }, { path: 'comments', populate: { path: 'user' } }]);
+    // example
+    // .populate([
+    //     { path: "user", select: "name fullName"},
+    //     {
+    //         path: "comments",
+    //         select : "content",
+    //         match : { user : "lkadsnfalskdnf"},
+    //         populate: { path: "user", select : "name fullName"}
+    //     }
+    // ])
+    return res.status(200).send({ blogs })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err: err.message })
+  }
+})
 
-blogRouter.post('/', async (req, res) => {
-    try {
-        const { title, content, islive, userId } = req.body;
-        if (typeof title !== 'string') return res.status(400).send({ err: 'title must be string' });
-        if (typeof content !== 'string') return res.status(400).send({ err: 'content must be string' });
-        if (islive && typeof islive !== 'boolean') return res.status(400).send({ err: 'live must be boolean' });
-        if (!isValidObjectId(userId)) return res.status(400).send({ err: 'user is invaild ' });
+blogRouter.post("/", async (req, res) => {
+  try {
+    const { title, content, islive, userId } = req.body
+    if (typeof title !== "string") return res.status(400).send({ err: "title must be string" })
+    if (typeof content !== "string") return res.status(400).send({ err: "content must be string" })
+    if (islive && typeof islive !== "boolean")
+      return res.status(400).send({ err: "live must be boolean" })
+    if (!isValidObjectId(userId)) return res.status(400).send({ err: "user is invaild " })
 
-        let user = await User.findById(userId);
-        //console.log(user); // 유저가 없으면 null 을 return 한다 -> Javascript 에서는 null 은 거짓값
-        if (!user) res.status(404).send({ err: 'user does not exist' });
-        // 참조가 되는 path 을 반드시 객체 형식으로 작성을 해줘야 합
-        let blog = new Blog({ ...req.body, user });
-        await blog.save();
-        return res.status(200).send({ blog });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ err: err.message });
-    }
-});
+    let user = await User.findById(userId)
+    //console.log(user); // 유저가 없으면 null 을 return 한다 -> Javascript 에서는 null 은 거짓값
+    if (!user) res.status(404).send({ err: "user does not exist" })
+    // 참조가 되는 path 을 반드시 객체 형식으로 작성을 해줘야 합
+    let blog = new Blog({ ...req.body, user })
+    await blog.save()
+    return res.status(200).send({ blog })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err: err.message })
+  }
+})
 
-blogRouter.get('/:blogId', async (req, res) => {
-    try {
-        const { blogId } = req.params;
-        if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blog is invaild ' });
-        const blog = await Blog.findOne({ _id: blogId });
-        return res.status(200).send({ blog });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ err: err.message });
-    }
-});
+blogRouter.get("/:blogId", async (req, res) => {
+  try {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId)) res.status(400).send({ err: "blog is invaild " })
+    const blog = await Blog.findOne({ _id: blogId })
+    const commentCount = await Comment.find({ blog: blogId }).countDocuments()
+    return res.status(200).send({ blog })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err: err.message })
+  }
+})
 
-blogRouter.put('/:blogId', async (req, res) => {
-    try {
-        const { blogId } = req.params;
-        if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blog is invaild ' });
-        const { title, content } = req.body;
-        if (typeof title !== 'string') res.status(400).send({ err: 'title must be string' });
-        if (typeof content !== 'string') res.status(400).send({ err: 'content must be string' });
+blogRouter.put("/:blogId", async (req, res) => {
+  try {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId)) res.status(400).send({ err: "blog is invaild " })
+    const { title, content } = req.body
+    if (typeof title !== "string") res.status(400).send({ err: "title must be string" })
+    if (typeof content !== "string") res.status(400).send({ err: "content must be string" })
 
-        const updateBlog = await Blog.findByIdAndUpdate(blogId, { title, content }, { new: true });
-        return res.status(200).send({ updateBlog });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ err: err.message });
-    }
-});
+    const updateBlog = await Blog.findByIdAndUpdate(blogId, { title, content }, { new: true })
+    return res.status(200).send({ updateBlog })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err: err.message })
+  }
+})
 
-blogRouter.patch('/:blogId/live', async (req, res) => {
-    try {
-        const { blogId } = req.params;
-        if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blog is invaild ' });
-        const { islive } = req.body;
-        console.log(islive);
-        if (islive && typeof islive !== 'boolean') res.status(400).send({ err: 'live must be boolean' });
-        const updateLive = await Blog.findOneAndUpdate(blogId, { islive }, { new: true });
-        return res.status(200).send({ updateLive });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ err: err.message });
-    }
-});
+blogRouter.patch("/:blogId/live", async (req, res) => {
+  try {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId)) res.status(400).send({ err: "blog is invaild " })
+    const { islive } = req.body
+    console.log(islive)
+    if (islive && typeof islive !== "boolean") res.status(400).send({ err: "live must be boolean" })
+    const updateLive = await Blog.findOneAndUpdate(blogId, { islive }, { new: true })
+    return res.status(200).send({ updateLive })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err: err.message })
+  }
+})
 
-blogRouter.delete('/:blogId', async (req, res) => {
-    try {
-        const { blogId } = req.params;
-        if (!isValidObjectId(blogId)) res.status(400).send({ err: 'Blog is valid' });
-        const blog = await Blog.findById({ _id: blogId });
-        if (!blog) res.status(404).send({ err: 'blog id not Found ' });
-        await Blog.deleteOne({ _id: blogId });
-        return res.status(200).send({ msg: 'delete Blog' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ err: err.message });
-    }
-});
+blogRouter.delete("/:blogId", async (req, res) => {
+  try {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId)) res.status(400).send({ err: "Blog is valid" })
+    const blog = await Blog.findById({ _id: blogId })
+    if (!blog) res.status(404).send({ err: "blog id not Found " })
+    await Blog.deleteOne({ _id: blogId })
+    return res.status(200).send({ msg: "delete Blog" })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ err: err.message })
+  }
+})
 
 module.exports = {
-    blogRouter,
-};
+  blogRouter,
+}
--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -1,73 +1,97 @@
-const { Router } = require('express');
-const commentRouter = Router({ mergeParams: true });
-const { Blog, Comment, User } = require('../models');
-const { isValidObjectId } = require('mongoose');
+const { Router } = require("express")
+const commentRouter = Router({ mergeParams: true })
+const { Blog, Comment, User } = require("../models")
+const { isValidObjectId, startSession } = require("mongoose")
 
-commentRouter.get('/', async (req, res) => {
-    let { page = 0 } = req.query;
-    page = parserInt(page);
-    const { blogId } = req.params;
-    if (!isValidObjectId(blogId)) res.status(400).send({ err: 'blog is invalid' });
-    const comments = await Comment.find({ blog: blogId })
-        .sort({ createdAt: -1 })
-        .skip(page * 3)
-        .limit(3);
-    return res.status(200).send({ comments });
-});
+commentRouter.get("/", async (req, res) => {
+  let { page = 0 } = req.query
+  page = parseInt(page)
+  const { blogId } = req.params
+  if (!isValidObjectId(blogId)) res.status(400).send({ err: "blog is invalid" })
+  const comments = await Comment.find({ blog: blogId })
+    .sort({ createdAt: -1 })
+    .skip(page * 3)
+    .limit(3)
+  return res.status(200).send({ comments })
+})
 
-commentRouter.post('/', async (req, res) => {
-    try {
-        const { blogId } = req.params;
-        if (!isValidObjectId(blogId)) return res.status(400).send({ err: 'blog is invalid' });
-        const { userId, content } = req.body;
-        if (!isValidObjectId(userId)) return res.status(400).send({ err: 'user is invalid' });
-        if (typeof content !== 'string') return res.status(400).send({ err: 'comment must be string' });
+commentRouter.post("/", async (req, res) => {
+  // 트랜잭션 적용
+  const session = await startSession()
+  let comment
+  try {
+    //await session.withTransaction(async () => {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId)) return res.status(400).send({ err: "blog is invalid" })
+    const { userId, content } = req.body
+    if (!isValidObjectId(userId)) return res.status(400).send({ err: "user is invalid" })
+    if (typeof content !== "string") return res.status(400).send({ err: "comment must be string" })
 
-        // const user = await User.findById(userId);
-        // const blog = await Blog.findById(blogId);
-        // 응답시간 개선하기
-        const [blog, user] = await Promise.all([Blog.findById(blogId), User.findById(userId)]);
-        console.log(`${user}  : ${blog}`);
+    // const user = await User.findById(userId);
+    // const blog = await Blog.findById(blogId);
+    // 응답시간 개선하기
+    const [blog, user] = await Promise.all([Blog.findById(blogId), User.findById(userId)])
+    //console.log(`${user}  : ${blog}`)
 
-        if (!user || !blog) return res.status(404).send({ err: 'blog or user is not exist' });
-        if (!blog.islive) return res.status(400).send({ err: 'blog is not available ' });
-        const comment = new Comment({
-            blog,
-            user,
-            userFullName: `${user.name.first} ${user.name.last}`,
-            content,
-        });
-        // await Promise.all([
-        //   comment.save(),
-        //   Blog.updateOne({ _id: blogId }, { $push: { comments: comment } }),
-        // ])
-        await comment.save();
+    if (!user || !blog) return res.status(404).send({ err: "blog or user is not exist" })
+    if (!blog.islive) return res.status(400).send({ err: "blog is not available " })
+    comment = new Comment({
+      blog: blogId,
+      user,
+      userFullName: `${user.name.first} ${user.name.last}`,
+      content,
+    })
+    // await session.abortTransaction()
 
-        return res.status(200).send({ comment });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send({ err: err.message });
-    }
-});
+    // 순환 참조
+    // {
+    //   "err": "Maximum call stack size exceeded"
+    // }
+    // await Promise.all([
+    //   comment.save(),
+    //   Blog.updateOne({ _id: blogId }, { $push: { comments: comment } }),
+    // ])
+    // blog.commentsCount++
+    // blog.comments.push(comment)
+    // if (blog.commentsCount > 3) blog.comments.shift()
 
-commentRouter.patch('/:commentId', async (req, res) => {
-    const { commentId } = req.params;
-    const { content } = req.body;
-    if (typeof content !== 'string') {
-        return res.status(400).send({ err: 'content is required' });
-    }
-    const [comment] = await Promise.all([
-        Comment.findByIdAndUpdate({ _id: commentId }, { content }, { new: true }),
-        Blog.updateOne({ 'comments._id': commentId }, { 'comments.$.content': content }),
-    ]); // 복잡한 문서를 document를 수정 삭제가 쉽다
-    return res.send({ comment });
-});
+    // await Promise([comment.save(), Blog.updateOne({ _id: blogId }, { $inc: { commentsCount: 1 } })])
+    //await Promise.all([comment.save(), blog.save()])
+    //})
+    await Promise.all([
+      comment.save(),
+      Blog.updateOne(
+        { _id: blogId },
+        { $inc: { commentsCount: 1 }, $push: { comments: { $each: [comment], $slice: -3 } } } // 제일 최근내용을 3개 빼고 잘라내라
+      ),
+    ])
+    return res.status(200).send({ comment })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send({ err: err.message })
+  } finally {
+    await session.endSession()
+  }
+})
 
-commentRouter.delete('/:commentId', async (req, res) => {
-    const { commentId } = req.params;
-    const comment = await Comment.findOneAndDelete({ _id: commentId });
-    await Blog.updateOne({ 'comments._id': commentId }, { $pull: { comments: { _id: commentId } } });
-    return res.send({ comment });
-});
+commentRouter.patch("/:commentId", async (req, res) => {
+  const { commentId } = req.params
+  const { content } = req.body
+  if (typeof content !== "string") {
+    return res.status(400).send({ err: "content is required" })
+  }
+  const [comment] = await Promise.all([
+    Comment.findByIdAndUpdate({ _id: commentId }, { content }, { new: true }),
+    Blog.updateOne({ "comments._id": commentId }, { "comments.$.content": content }),
+  ]) // 복잡한 문서를 document를 수정 삭제가 쉽다
+  return res.send({ comment })
+})
 
-module.exports = { commentRouter };
+commentRouter.delete("/:commentId", async (req, res) => {
+  const { commentId } = req.params
+  const comment = await Comment.findOneAndDelete({ _id: commentId })
+  await Blog.updateOne({ "comments._id": commentId }, { $pull: { comments: { _id: commentId } } })
+  return res.send({ comment })
+})
+
+module.exports = { commentRouter }
